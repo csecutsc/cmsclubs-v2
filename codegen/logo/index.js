98037b328ed3874031507e0ec8b71e4fd4d5fbfb
fpunny <frederic.pun@gmail.com>
2020-09-23 01:28:21 -0400

    Fixed codgen

--- codegen/logo/index.js
+++ codegen/logo/index.js
@@ -6,15 +6,17 @@ const fss = require('fs');
 module.exports = async function () {
   const [ svg ] = await Promise.all([
     fs.readFile(path.join(__dirname, `logo.svg`))
-      .then((content) => content.toString().replace(/\$COLOR/g, PRIMARY_COLOR)),
-    new Promise(resolve => {
+      .then((content) => content.toString().replace(/\$COLOR/g, PRIMARY_COLOR))
+    ,
+    new Promise(async resolve => {
       if (!fss.existsSync(`./static`)) {
-        resolve(fs.mkdir(`./static`));
+        await fs.mkdir(`./static`);
       }
+      resolve();
     }),
   ]);
 
-  await Promise.all([
+  return Promise.all([
     fs.writeFile(`./src/images/logo.svg`, svg),
     fs.writeFile(`./static/logo.svg`, svg),
   ]);
