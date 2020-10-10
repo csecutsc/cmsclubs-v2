8826aa58085558ddcd2a60a029252b4d53f08667
fpunny <frederic.pun@gmail.com>
2020-10-10 19:12:58 -0400

    slimming env for AWS

--- codegen/googleAuth/index.js
+++ codegen/googleAuth/index.js
@@ -1,9 +1,19 @@
+const { exec } = require(`child_process`);
 const fs = require(`fs/promises`);
 require(`dotenv`).config();
 
-module.exports = function () {
-  return fs.writeFile(
+module.exports = async function () {
+  await fs.writeFile(
     process.env.GOOGLE_CREDENTIALS_FILE,
     process.env.GOOGLE_CREDENTIALS,
   );
+  // To slim the environment variables down for AWS
+  return new Promise((resolve, reject) => {
+    exec(`unset GOOGLE_CREDENTIALS`, (error, stdout, stderr) => {
+      if (error || stderr) {
+        return reject(error || stderr);
+      }
+      resolve();
+    });
+  });
 };
